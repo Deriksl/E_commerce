1a3afee402bd06d5895efb7988c43955d6d96e32
Deriksl <133131434+Deriksl@users.noreply.github.com>
2023-12-12 07:18:47 -0600

    Delete E-commerce directory

--- E-commerce/02-Precio.sql
+++
@@ -1,9 +0,0 @@
-
-DROP TABLE IF EXISTS Ecommerce.Precio;
-
-CREATE TABLE Ecommerce.Precio (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    precio DECIMAL (10,2),
-    fecha DATE
-);
-
--- E-commerce/21-Pedido_Direccion.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS Ecommerce.Pedido_Direccion;
-
-CREATE TABLE Ecommerce.Pedido_Direccion (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    id_Pedido BIGINT NOT NULL,
-    id_Direccion BIGINT NOT NULL,
-    CONSTRAINT FK_PE FOREIGN KEY (id_Pedido) REFERENCES Ecommerce.Pedido(id),
-    CONSTRAINT FK_DR FOREIGN KEY (id_Direccion) REFERENCES Ecommerce.Direccion(id)
-);
