550964a450a7aaa57bcdde37b03f0a3cdeb42933
Deriksl <133131434+Deriksl@users.noreply.github.com>
2023-12-12 19:37:56 -0600

    Add files via upload

---
+++ E-commerce/03-Palabra_clave.sql
@@ -0,0 +1,58 @@
+
+DROP TABLE IF EXISTS Ecommerce.Palabra_clave;
+
+CREATE TABLE Ecommerce.Palabra_clave (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    palabra VARCHAR(255) NOT NULL
+);
+
+insert into Ecommerce.Palabra_clave (id, palabra) values (1, 'Tecnología portátil');
+insert into Ecommerce.Palabra_clave (id, palabra) values (2, 'Moda sostenible');
+insert into Ecommerce.Palabra_clave (id, palabra) values (3, 'Accesorios de cocina');
+insert into Ecommerce.Palabra_clave (id, palabra) values (4, 'Juegos de mesa');
+insert into Ecommerce.Palabra_clave (id, palabra) values (5, 'Cuidado de la piel natural');
+insert into Ecommerce.Palabra_clave (id, palabra) values (6, 'Equipamiento fitness');
+insert into Ecommerce.Palabra_clave (id, palabra) values (7, 'Juguetes educativos');
+insert into Ecommerce.Palabra_clave (id, palabra) values (8, 'Decoración del hogar');
+insert into Ecommerce.Palabra_clave (id, palabra) values (9, 'Instrumentos musicales');
+insert into Ecommerce.Palabra_clave (id, palabra) values (10, 'Electrónica de consumo');
+insert into Ecommerce.Palabra_clave (id, palabra) values (11, 'Productos de limpieza ecológicos');
+insert into Ecommerce.Palabra_clave (id, palabra) values (12, 'Maquillaje orgánico');
+insert into Ecommerce.Palabra_clave (id, palabra) values (13, 'Alimentos saludables');
+insert into Ecommerce.Palabra_clave (id, palabra) values (14, 'Arte y manualidades');
+insert into Ecommerce.Palabra_clave (id, palabra) values (15, 'Ropa deportiva de alto rendimiento');
+insert into Ecommerce.Palabra_clave (id, palabra) values (16, 'Joyería artesanal');
+insert into Ecommerce.Palabra_clave (id, palabra) values (17, 'Herramientas de jardinería');
+insert into Ecommerce.Palabra_clave (id, palabra) values (18, 'Equipos para camping');
+insert into Ecommerce.Palabra_clave (id, palabra) values (19, 'Suministros para mascotas');
+insert into Ecommerce.Palabra_clave (id, palabra) values (20, 'Libros electrónicos');
+insert into Ecommerce.Palabra_clave (id, palabra) values (21, 'Ropa para bebés y niños');
+insert into Ecommerce.Palabra_clave (id, palabra) values (22, 'Viajes y accesorios para exteriores');
+insert into Ecommerce.Palabra_clave (id, palabra) values (23, 'Arte digital descargable');
+insert into Ecommerce.Palabra_clave (id, palabra) values (24, 'Decoración de oficina');
+insert into Ecommerce.Palabra_clave (id, palabra) values (25, 'Suministros para manualidades DIY');
+insert into Ecommerce.Palabra_clave (id, palabra) values (26, 'Productos de bienestar');
+insert into Ecommerce.Palabra_clave (id, palabra) values (27, 'Suministros de pintura');
+insert into Ecommerce.Palabra_clave (id, palabra) values (28, 'Comida gourmet');
+insert into Ecommerce.Palabra_clave (id, palabra) values (29, 'Equipamiento para fotografía');
+insert into Ecommerce.Palabra_clave (id, palabra) values (30, 'Productos de higiene personal');
+insert into Ecommerce.Palabra_clave (id, palabra) values (31, 'Suministros de costura');
+insert into Ecommerce.Palabra_clave (id, palabra) values (32, 'Productos de cuidado del cabello');
+insert into Ecommerce.Palabra_clave (id, palabra) values (33, 'Productos de ferretería');
+insert into Ecommerce.Palabra_clave (id, palabra) values (34, 'Arte de pared');
+insert into Ecommerce.Palabra_clave (id, palabra) values (35, 'Calzado de moda');
+insert into Ecommerce.Palabra_clave (id, palabra) values (36, 'Ropa íntima especializada');
+insert into Ecommerce.Palabra_clave (id, palabra) values (37, 'Suministros de pesca');
+insert into Ecommerce.Palabra_clave (id, palabra) values (38, 'Dispositivos de almacenamiento de datos');
+insert into Ecommerce.Palabra_clave (id, palabra) values (39, 'Instrumentos científicos');
+insert into Ecommerce.Palabra_clave (id, palabra) values (40, 'Equipos de sonido y audio');
+insert into Ecommerce.Palabra_clave (id, palabra) values (41, 'Juguetes para mascotas');
+insert into Ecommerce.Palabra_clave (id, palabra) values (42, 'Gadgets tecnológicos');
+insert into Ecommerce.Palabra_clave (id, palabra) values (43, 'Equipos de protección personal');
+insert into Ecommerce.Palabra_clave (id, palabra) values (44, 'Suministros de yoga y meditación');
+insert into Ecommerce.Palabra_clave (id, palabra) values (45, 'Utensilios de cocina de diseño');
+insert into Ecommerce.Palabra_clave (id, palabra) values (46, 'Equipamiento para barbacoa');
+insert into Ecommerce.Palabra_clave (id, palabra) values (47, 'Juegos de construcción');
+insert into Ecommerce.Palabra_clave (id, palabra) values (48, 'Ropa vintage');
+insert into Ecommerce.Palabra_clave (id, palabra) values (49, 'Artículos de papelería');
+insert into Ecommerce.Palabra_clave (id, palabra) values (50, 'Productos para el cuidado del hogar');
